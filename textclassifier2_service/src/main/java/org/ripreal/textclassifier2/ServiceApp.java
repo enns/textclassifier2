8bcc0d5f799e667b282e3b4dbeb0256539601a60
ripreal <ripreal1@yandex.ru>
2018-03-17 16:39:03 +0300

    fix gradle

--- textclassifier2_service/src/main/java/org/ripreal/textclassifier2/ServiceApp.java
+++ textclassifier2_service/src/main/java/org/ripreal/textclassifier2/ServiceApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class ServiceApp {
     public static void main(String... args) {
         SpringApplication.run(ClassifierApp.class, args);
     }
