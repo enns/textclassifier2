ece49977e55dd292891cbbf394d5969346f97fca
ripreal <ripreal1@yandex.ru>
2018-04-02 22:50:00 +0300

    ALL DEF

--- textclassifier2_service/src/main/java/org/ripreal/textclassifier2/storage/testdata/JiraRESTWrapper.java
+++ textclassifier2_service/src/main/java/org/ripreal/textclassifier2/storage/testdata/JiraRESTWrapper.java
@@ -2,25 +2,17 @@ package org.ripreal.textclassifier2.storage.testdata;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.ripreal.textclassifier2.JiraBasicAuthClient;
-import org.ripreal.textclassifier2.JiraIssueReader;
-import org.ripreal.textclassifier2.PropertiesClient;
 import org.ripreal.textclassifier2.model.ClassifiableFactory;
 import org.ripreal.textclassifier2.model.ClassifiableText;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Configuration
 @Slf4j
 public class JiraRESTWrapper {
-
+/*
     @NonNull
     private final ClassifiableFactory textFactory;
 
@@ -48,5 +40,5 @@ public class JiraRESTWrapper {
         }
         return null;
     }
-
+*/
 }
